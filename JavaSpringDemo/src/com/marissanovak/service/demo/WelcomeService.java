09e72833b7dd8c371f1b2f31b19a96a59d4084e1
mnovak <marissa@getservice.com>
2017-12-13 17:07:02 -0800

    Java EE basics, JSP, JSTL, Spring IoC from file system, Spring IoC from classpath

---
+++ JavaSpringDemo/src/com/marissanovak/service/demo/WelcomeService.java
@@ -0,0 +1,17 @@
+package com.marissanovak.service.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WelcomeService {
+	public List<String> getWelcomeMessage(String name) {
+		List<String> myWelcomeMessage = new ArrayList<>();
+		
+		// Add data to the list
+		myWelcomeMessage.add("Hello! ");
+		myWelcomeMessage.add(name);
+		myWelcomeMessage.add(", welcome to my practice app");
+		
+		return myWelcomeMessage;
+	}
+}
---
+++ spring-ioc-demo-1/src/main/java/com/marissanovak/springdemo/Organization.java
@@ -0,0 +1,8 @@
+package com.marissanovak.springdemo;
+
+public class Organization {
+	public void corporateSlogan() {
+		String slogan = "We build the ultimate driving machines!";
+		System.out.println(slogan);
+	}
+}
